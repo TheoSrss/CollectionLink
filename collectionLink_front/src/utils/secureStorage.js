806007e7f8c5567b0b12732ad1de18cc26220d59
Théo <theo.sourisseau79@gmail.com>
2025-02-12 09:43:56 +0100

    delete SECRET_KEY for encrypte

--- collectionLink_front/src/utils/secureStorage.js
+++ collectionLink_front/src/utils/secureStorage.js
@@ -1,19 +1,12 @@
 import CryptoJS from 'crypto-js';
 
-const SECRET_KEY = process.env.REACT_APP_SECRET_KEY
-
 export const secureStorage = {
     setItem: (key, value) => {
-        const encrypted = CryptoJS.AES.encrypt(JSON.stringify(value), SECRET_KEY).toString();
-        localStorage.setItem(key, encrypted);
+        localStorage.setItem(key, JSON.stringify(value));
     },
     getItem: (key) => {
-        const encrypted = localStorage.getItem(key);
-        if (encrypted) {
-            const decrypted = CryptoJS.AES.decrypt(encrypted, SECRET_KEY).toString(CryptoJS.enc.Utf8);
-            return JSON.parse(decrypted);
-        }
-        return null;
+        const value = localStorage.getItem(key);
+        return value ? JSON.parse(value) : null;
     },
     removeItem: (key) => {
         localStorage.removeItem(key);
