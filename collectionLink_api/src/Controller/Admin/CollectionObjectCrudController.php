70bdc04d02f06b0afb95a7a8cbd76affe6dc03ac
Théo <theo.sourisseau79@gmail.com>
2025-02-20 16:10:38 +0100

    fix | collection forn easyAdmin

--- collectionLink_api/src/Controller/Admin/CollectionObjectCrudController.php
+++ collectionLink_api/src/Controller/Admin/CollectionObjectCrudController.php
@@ -5,39 +5,29 @@ namespace App\Controller\Admin;
 use App\Entity\CollectionObject;
 use App\Service\EntityProcessorService;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Bundle\SecurityBundle\Security;
 
 class CollectionObjectCrudController extends AbstractCrudController
 {
     public function __construct(
-        private readonly EntityProcessorService $customPersistService)
-    {
-    }
+        private readonly EntityProcessorService $customPersistService,
+        private Security $security
+    ) {}
 
     public static function getEntityFqcn(): string
     {
         return CollectionObject::class;
     }
 
-
     public function configureFields(string $pageName): iterable
     {
         return [
             TextField::new('name'),
             AssociationField::new('user')->autocomplete(),
-            AssociationField::new('collectable')
-                ->autocomplete()
-                ->setQueryBuilder(
-                    fn(QueryBuilder $queryBuilder) => $queryBuilder
-                        ->andWhere('entity.public = :isPublic')
-                        ->setParameter('isPublic', true)
-                )
+            AssociationField::new('collectable')->autocomplete()
         ];
     }
 
